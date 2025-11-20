39f5ac0da2fa0858544e634dec59cf778f60d119
Hani <hanider27@gmail.com>
2025-11-20 08:32:28 +0100

    mise a jours

--- QRcode/forms.py
+++ QRcode/forms.py
@@ -2,19 +2,19 @@ from django import forms
 
 
 class QRCodeForm(forms.Form):
-    restaurant_name = forms.CharField(
+    QR_code_name = forms.CharField(
         max_length=50,
-        label='Restaurant Name',
+        label='QR name',
         widget=forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Enter Restaurant name'
+            'placeholder': 'Enter QR name'
         })
         )
     url = forms.URLField(
         max_length=200, 
-        label='Menu URL',
+        label='Website URL',
         widget=forms.URLInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Enter the URL of your online menu'
+            'placeholder': 'Enter the URL of your online website'
         })
         )
